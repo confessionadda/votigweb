acda7674a589523066fcadb8ee2dec5dc126903e
confessionadda <confessionsggs274@gmail.com>
2025-05-20 21:48:33 +0530

    Update vote.js

--- vote.js
+++ vote.js
@@ -1,5 +1,5 @@
 // ✅ 1. Set current game name here — change this for every new game
-const gameName = 'crush'; // eg: 'crush', 'handsome', 'funny', etc.
+const gameName = 'smart'; // eg: 'crush', 'handsome', 'funny', etc.
 
 // ✅ 2. Load nominees and handle vote status per game
 function loadNominees() {
